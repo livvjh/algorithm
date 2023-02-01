7d8fdb7e81004d2894fe8431d831f9f94d6cda54
livvjh <livejh7129@gmail.com>
2023-02-01 22:25:51 +0900

    Runtime: 66 ms(72.99%) | Memory: 15.1 MB (42.82%)

---
+++ leetcode/1_two_sum/__init__.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+# 1회 풀이
+class Solution:
+    @staticmethod
+    def two_sum(nums: List[int], target: int) -> List[int]:
+        if len(nums) > 1:
+            result = []
+            if len(nums) == 2 and sum(nums) == target:
+                return [0, 1]
+            for i in range(len(nums)):
+                temp_list = nums.copy()
+                root = nums[i]
+                del temp_list[i]
+                for j in range(len(temp_list)):
+                    if target == root + temp_list[j]:
+                        result.append(i)
+                        result.append(j + 1)
+                        return result
+
+
+# 2회 풀이
+class Solution2:
+    @staticmethod
+    def two_sum(nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i in range(len(nums)):
+            nums_dict[nums[i]] = i
+        for i in range(len(nums)):
+            find_key = target - nums[i]
+            if find_key in nums_dict and nums_dict[find_key] != i:
+                return [i, nums_dict[find_key]]
+
+
+print(Solution2.two_sum([3, 3], 6))
