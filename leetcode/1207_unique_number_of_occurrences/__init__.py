f5c629e335d367755ae17b3bc0f34ef957cc1ec8
livvjh <livejh7129@gmail.com>
2023-02-17 07:31:00 +0900

    34 ms(90.65%) | Memory: 13.9 MB (65.45%)

--- leetcode/1207_unique_number_of_occurrences/__init__.py
+++ leetcode/1207_unique_number_of_occurrences/__init__.py
@@ -3,12 +3,19 @@ from typing import List
 
 class Solution:
     def unique_occurrences(self, arr: List[int]) -> bool:
-        # 주어진 배열의 값의 갯수가 고유하거나 그렇지 않으면 false
-        # 고유하지 않으면 true
-        set_arr = set(arr)
-        if len(arr) == len(set_arr):
-            return False
-        return True
+        # 주어진 배열의 count가 unique한지 체크
+        # unique면 true else false
+        temp_dict = {}
+        for i,v in enumerate(arr):
+            if temp_dict.get(v):
+                temp_dict[v] += 1
+            else:
+                temp_dict[v] = 1
+        count_list = temp_dict.values()
+        set_count_list = set(count_list)
+        if len(count_list) == len(set_count_list):
+            return True
+        return False
 
 
 print(Solution().unique_occurrences([1, 2, 2, 1, 1, 3]))
