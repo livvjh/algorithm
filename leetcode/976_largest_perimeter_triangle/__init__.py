f2b9a98e7365639530842e4c1f48bf8b8ac02eae
livvjh <livejh7129@gmail.com>
2023-03-02 19:40:58 +0900

    191 ms (89.40%) | Memory: 15.4 MB (38.69%)

--- leetcode/976_largest_perimeter_triangle/__init__.py
+++ leetcode/976_largest_perimeter_triangle/__init__.py
@@ -5,21 +5,18 @@ class Solution:
     def largest_perimeter(self, nums: List[int]) -> int:
         #  a + b > c
         nums.sort()
-        print(nums)
-        index, result = 0, 0
-        while index + 2 < len(nums):
-            triangle = nums[index:index + 3]
-            if triangle[0] + triangle[1] >= triangle[2]:
-                index += 1
-                result = sum(triangle)
-            else:
-                return result
+        size = len(nums)
+        result = 0
+        for i in range(size - 3, -1, -1):  # 큰 수 찾으면 그냥 break return (reversed로 반복)
+            if nums[i] + nums[i + 1] > nums[i + 2]:
+                result = nums[i] + nums[i + 1] + nums[i + 2]
+                break
         return result
 
-    # print(Solution().largest_perimeter([2, 1, 2]))
 
+print(Solution().largest_perimeter([2, 1, 2]))
 
-print(Solution().largest_perimeter([1, 4, 18, 3, 8, 4, 4]))
+# print(Solution().largest_perimeter([1, 4, 18, 3, 8, 4, 4]))
 # print(Solution().largest_perimeter([1, 2, 1, 10]))
 # print(Solution().largest_perimeter([3, 2, 3, 4]))
 # print(Solution().largest_perimeter([3, 6, 2, 3]))
