bc683e78a95a44b820060821d74e8d9ed3dc49b3
james <james@soomgo.com>
2023-04-10 11:48:39 +0900

    424 init

---
+++ leetcode/424_longest_repeating_character_replacement/__init__.py
@@ -0,0 +1,7 @@
+class Solution:
+    def character_replacement(self, s: str, k: int) -> int:
+        pass
+
+
+print(Solution().character_replacement(s="AABABBA", k=1))
+print(Solution().character_replacement(s="ABAB", k=2))
